fceb57e4b7366143f423264a9305982b927a996c
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2020-04-15 22:56:41 -0700

    Create film instance

---
+++ FilmLocations/FilmEntry.swift
@@ -0,0 +1,16 @@
+//
+//  FilmEntry.swift
+//  FilmLocations
+//
+//  Created by Angelina Olmedo on 4/15/20.
+//  Copyright © 2020 Angelina Olmedo. All rights reserved.
+//
+
+import Foundation
+
+struct FilmEntry {
+    var firstActor: String
+    var locations: String
+    var releaseYear: String
+    var title: String
+}
--- FilmLocations/ViewController.swift
+++ FilmLocations/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var films:[FilmEntry] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,11 +29,23 @@ class ViewController: UIViewController {
                 if let data = contents,
                     let jsonResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [[String:Any]] {
                 print(jsonResult)
+                for film in jsonResult{
+                    let firstActor = film["actor_1"] as? String ?? ""
+                    let locations = film["locations"] as? String  ?? ""
+                    let releaseYear = film["release_year"] as? String  ?? ""
+                    let title = film["title"] as? String  ?? ""
+                    let movie = FilmEntry(firstActor: firstActor, locations: locations, releaseYear: releaseYear, title: title)
+                    
+                    films.append(movie)
+                }
             }
             } catch {
                 print("Error deserializing JSON: \(error)")
             }
+            
+            
         }
+        
     }
 
 
